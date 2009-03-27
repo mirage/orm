9fe1eca0177f675a2f1b6f1673f57e1bdcc93555
Anil Madhavapeddy <anil@recoil.org>
2009-03-27 21:54:04 +0000

    add in the SQL access utility library; will eventually inline this to the generated code

---
+++ sql_access.ml
@@ -0,0 +1,32 @@
+open Sqlite3
+open Printf
+
+exception Sql_error of (Rc.t * string)
+
+let raise_sql_error x =
+    raise (Sql_error (x, (Rc.to_string x)))
+
+let try_finally fn finalfn =
+    try
+      let r = fn () in
+      finalfn ();
+      r
+    with e -> begin
+      print_endline "WARNING: exception";
+      finalfn ();
+      raise e
+    end
+
+(* retry until a non-BUSY error code is returned *)
+let rec db_busy_retry fn =
+    match fn () with
+    |Rc.BUSY -> 
+       print_endline "WARNING: busy";
+       Thread.delay (Random.float 1.);
+       db_busy_retry fn
+    |x -> x
+
+let db_must_ok fn =
+    match db_busy_retry fn with
+    |Rc.OK -> ()
+    |x -> raise_sql_error x
