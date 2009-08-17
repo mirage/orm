c76e8b56ae24af97cbe19c3b7f45ef0307ce1d6e
Anil Madhavapeddy <anil@recoil.org>
2009-08-18 00:37:59 +0100

    adapt the camlp4 for the sql_types generation; still pretty broken but getting there

--- pa_sql_orm.ml
+++ pa_sql_orm.ml
@@ -56,199 +56,79 @@ let function_with_label_args _loc ~fun_name ~final_ident ~function_body ~return_
        ) opt_args <:expr< ( $function_body$ : $return_type$ ) >>
       $ >>
    
-(* Return the fields of a record/object type *)
-let fields_of_record t =
-  match t.td_typ with
-  |Record (_,fl) |Object (_,fl) ->  fl
-  |_ -> failwith "fields_of_record: unexpected type"
+(* convert an exposed ocaml type into an AST ctyp fragment *)
+let rec ast_of_caml_type _loc = function
+  |Types.Unit _    -> <:ctyp< unit >>
+  |Types.Int _     -> <:ctyp< int >>
+  |Types.Int32 _   -> <:ctyp< int32 >>
+  |Types.Int64 _   -> <:ctyp< int64 >>
+  |Types.Float _   -> <:ctyp< float >>
+  |Types.Char _    -> <:ctyp< char >>
+  |Types.String _  -> <:ctyp< string >>
+  |Types.Bool _    -> <:ctyp< bool >>
+  |Types.List (_,ty) -> <:ctyp< list $ast_of_caml_type _loc ty$ >>
+  |Types.Array (_,ty) -> <:ctyp< array $ast_of_caml_type _loc ty$ >>
+  |Types.Option (_,ty) -> <:ctyp< option $ast_of_caml_type _loc ty$ >>
+  |x -> failwith ("ast_of_caml_type: " ^ (Types.string_of_typ x))
 
-(* Convert an OCaml type into the value exposed in the persist object.
-   Not just directly passing through the original type so we can throw
-   an error explicitly if its not supported *)
-exception Unsupported_type of string
-let unsupported ty = raise (Unsupported_type ty)
-
-type sql_type = {
-  s_opt: bool;     (* is this type optional *)
-  s_foreign: bool; (* is this type a foreign key *)
-  s_list: bool;    (* is this type a list *)
-}
-
-let classify_type _loc f =
-  let simple_classify _loc sty = function
-    |Int _ ->    f, sty, <:ctyp< int >>
-    |Int32 _ ->  f, sty, <:ctyp< int32 >>
-    |Int64 _ ->  f, sty, <:ctyp< int64 >>
-    |Float _ ->  f, sty, <:ctyp< float >>
-    |Bool _ ->   f, sty, <:ctyp< bool >>
-    |Char _ ->   f, sty, <:ctyp< char >>
-    |String _ -> f, sty, <:ctyp< string >>
-    |Apply (_loc, _, id, _) -> 
-      let sty = { sty with s_foreign=true } in
-      f, sty, <:ctyp< $lid:id$ >>
-    |x -> unsupported (Types.string_of_typ x)
-  in
-  (* decompose a list or option type into the basic type and set options *)
-  let base = { s_opt=false; s_foreign=false; s_list=false } in
-  match f.f_typ with
-  |Option (_, ty) -> simple_classify _loc { base with s_opt=true } ty
-  |List (_, ty) ->   simple_classify _loc { base with s_list=true } ty
-  |ty ->             simple_classify _loc base ty
-  
-(* convert a simple ocaml type into a sql type *)
-let sql_type_of_ocaml_type = function
-  | <:ctyp< int >> 
-  | <:ctyp< int32 >> 
-  | <:ctyp< int64 >>  -> "INTEGER"
-  | <:ctyp< float >>  -> "REAL"
-  | <:ctyp< bool >> 
-  | <:ctyp< char >>   -> "INTEGER"
-  | <:ctyp< string >> -> "TEXT"
-  | x -> unsupported  "sql_type_of_ocaml_type:"
-
-let filter_fields _loc fn fl =
-  let cltys = List.map (classify_type _loc) fl in
-  List.filter (fun (_,s,_) -> fn s) cltys
-  
-(* get the basic fields (i.e. not lists, options, foreign) from a list *)
-let get_basic_fields _loc fl =
-  filter_fields _loc (fun s -> s.s_foreign = false && s.s_list = false) fl
-
-let get_foreign_fields _loc fl =
-  filter_fields _loc (fun s -> s.s_foreign = true || s.s_list = true) fl
-
-let get_non_list_foreign_fields _loc fl =
-  filter_fields _loc (fun s -> s.s_foreign = true && s.s_list = false) fl
- 
-let get_list_foreign_fields _loc fl =
-  filter_fields _loc (fun s -> s.s_list = true) fl
-
-let is_basic_type _loc ty =
-  let _,s,_ = classify_type _loc ty in
-  match s with
-  |{s_opt=false; s_list=false; s_foreign=false } -> true
-  |_ -> false 
-
-(* Return the accessor typedefs (the method/set_method) for a 
-   particular OCaml type *)
-let accessor_funcs_of_typedef _loc f =
-  prerr_endline (Types.string_of_typ f.f_typ);
-  let f, sty, ty = classify_type _loc f in
-  prerr_endline (sprintf "%s: opt=%b list=%b for=%b" f.f_id sty.s_opt sty.s_list sty.s_foreign);
-  let ty = match sty with
-    |{ s_opt = true } -> <:ctyp< option $ty$ >>
-    |{ s_list = true } -> <:ctyp< list $ty$ >>
-  |_ -> ty in
-  let id = f.f_id in
-  let set_id = sprintf "set_%s" id in
-  let acc = <:ctyp< $lid:id$ : $ty$ >> in
-  let set_acc = <:ctyp< $lid:set_id$ : $ty$ -> unit >> in
-  [ acc; set_acc ]
+open Sql_types
 
 (* declare the types of the _persist objects used to pass SQL
-   objects back and forth*)
-let construct_typedefs tds =
+   objects back and forth *)
+let construct_typedefs env =
   let _loc = Loc.ghost in
-  let ts = parse_typedef _loc tds in
-  let object_decls = Ast.tyAnd_of_list (List.map (fun td ->
-    let ts_name = td.td_id ^ "_persist" in
-    let ts_fields = fields_of_record td in
-    let fields = List.flatten (List.map (accessor_funcs_of_typedef _loc) ts_fields) in
-    let other_fields = List.map (fun i -> <:ctyp< $lid:i$ : unit >>) ["save"; "delete"] in
-    let all_fields = fields @ other_fields in
-    declare_type _loc ts_name <:ctyp< < $list:all_fields$ > >>;
-  ) ts) in
+  let tables = exposed_tables env in
+  let object_decls = Ast.tyAnd_of_list (List.fold_left (fun decls t ->
+    (* define the external type name as <name>_persist *)
+    let ts_name = t.t_name ^ "_persist" in
+    (* define the accessor and set_accessor functions *)
+    let fields = exposed_fields env t.t_name in
+    let accessor_fields = List.flatten (List.map (fun (ty,f) ->
+      let ctyp = ast_of_caml_type _loc ty in
+      match f.f_opt with
+      |true -> [
+       <:ctyp< $lid:f.f_name$ : option $ctyp$ >> ;
+       <:ctyp< $lid:"set_" ^ f.f_name$ : option $ctyp$ -> unit >> ]
+      |false -> [
+       <:ctyp< $lid:f.f_name$ : $ctyp$ >> ;
+       <:ctyp< $lid:"set_" ^ f.f_name$ : $ctyp$ -> unit >> ]
+    ) fields) in
+    let other_fields = List.map (fun i ->
+      <:ctyp< $lid:i$ : unit >>) ["save"; "delete"] in
+    let all_fields = accessor_fields  @ other_fields in
+    declare_type _loc ts_name <:ctyp< < $list:all_fields$ > >> :: decls;
+  ) [] tables) in
   <:str_item< type $object_decls$ >> 
 
-let table_name_of n = "table_" ^ n
-let list_table_name_of t f = sprintf "table_%s__list_%s" t f
-
-let construct_funs tds =
+(* construct the functions to init the db and create objects *)
+let construct_funs env =
   let _loc = Loc.ghost in
-  let ts = parse_typedef _loc tds in
+  let tables = exposed_tables env in
   (* output the function bindings *)
-  let fn_bindings = Ast.biAnd_of_list (List.map (fun td ->
-
+  let fn_bindings = Ast.biAnd_of_list (List.map (fun t ->
     (* init function to initalize sqlite database *)
-    let type_name = sprintf "%s_persist" td.td_id in
-    let fun_name = sprintf "%s_init_db" td.td_id in
+    let type_name = sprintf "%s_persist" t.t_name in
+    let fun_name = sprintf "%s_init_db" t.t_name in
 
-    let fields = fields_of_record td in
-    let basic_fields = get_basic_fields _loc fields in
-    let non_list_foreign_fields = get_non_list_foreign_fields _loc fields in
-    let list_foreign_fields = get_list_foreign_fields _loc fields in
+    let sql_decls = List.fold_left (fun a t ->
+      let table = find_table env t in
+      let fields = sql_fields env t in
+      let sql_fields = List.map (fun f ->
+        sprintf "%s %s" f.f_name (string_of_sql_type f.f_typ)
+      ) fields in
+      sprintf "CREATE TABLE IF NOT EXISTS %s (%s)" t (String.concat ", " sql_fields) :: a
+    ) [] (t.t_name :: t.t_child) in
 
-    let pid = "id INTEGER PRIMARY KEY AUTOINCREMENT" in
-    let sql_decls =  (* the list of sql create statements *)
-         pid  (* the id key *)
-      :: (List.map (fun (f,_,oty) ->
-          sprintf "%s %s" f.f_id (sql_type_of_ocaml_type oty)
-        ) basic_fields) (* basic field decls *)
-      @ (* also include any non-list foreign mappings in this table *)
-        (List.map (fun (f,sql_ty,oty) ->
-          sprintf "%s_id INTEGER" (table_name_of f.f_id)
-          ) non_list_foreign_fields
-        )
-    in
-    let basic_sql = sprintf "CREATE TABLE IF NOT EXISTS %s (%s);"
-      (table_name_of td.td_id) (String.concat ", " sql_decls) in
-    let foreign_many_sql = List.map (fun (f,sql_ty,oty) ->
-        let td_tname = table_name_of td.td_id in
-        match sql_ty with
-        |{ s_foreign=true } ->
-          let fl_tname = table_name_of f.f_id in
-          sprintf "CREATE TABLE IF NOT EXISTS %s (%s_id INTEGER, %s_id INTEGER, PRIMARY KEY(%s_id, %s_id));" 
-            td_tname td_tname fl_tname td_tname fl_tname
-        |{ s_list=true; s_foreign=false }  ->
-          let td_list_tname = list_table_name_of td.td_id f.f_id in
-          sprintf "CREATE TABLE IF NOT EXISTS %s (%s_id INTEGER PRIMARY KEY, %s %s);"
-            td_list_tname td_tname f.f_id (sql_type_of_ocaml_type oty)
-        |_ -> failwith "unknown foreign case"
-      ) list_foreign_fields in
-    let all_create_sql = basic_sql :: foreign_many_sql in
-    (* fragment to create table in db *)
-    let create_table sql_text = <:expr<
-        let sql = $str:sql_text$ in
+    let create_table sql = <:expr<
+        let sql = $str:sql$ in
         Sql_access.db_must_ok db (fun () -> Sqlite3.exec db.Sql_access.db sql)
      >> in
-    let create_statements = Ast.exSem_of_list (List.map create_table all_create_sql) in
+    let create_statements = Ast.exSem_of_list (List.map create_table sql_decls) in
     let init_db_binding = <:binding< $lid:fun_name$ db = $create_statements$ >> in
-
-    (* new function to create object of the type *)
-    let fun_name = sprintf "%s_new" td.td_id in
-    let opt_args = <:patt< ?(id=None) >> :: (List.map (fun f ->
-        <:patt< ~ $lid:f.f_id$ >>
-      ) fields) in
-    let new_get_set_functions = List.flatten (List.map (fun f ->
-       let internal_var_name = sprintf "_%s" f.f_id in
-       let external_var_name = f.f_id in
-       [ 
-         <:class_str_item< value mutable $lid:internal_var_name$ = $lid:external_var_name$ >>;
-         <:class_str_item< method $lid:external_var_name$ = $lid:internal_var_name$ >>;
-         <:class_str_item< method $lid:"set_"^external_var_name$ v = ( $lid:internal_var_name$ := v ) >>;
-       ]
-    ) fields) in
-    let new_admin_functions =
-       [
-         <:class_str_item< method delete = prerr_endline "delete" >>;
-         <:class_str_item< method save   = prerr_endline "save" >>;
-       ] in
-    let new_functions = new_get_set_functions @ new_admin_functions in
-    let new_body = <:expr<
-        object
-         $Ast.crSem_of_list new_functions$;
-        end
-      >> in
-    let new_binding = function_with_label_args _loc 
-      ~fun_name 
-      ~final_ident:"db"
-      ~function_body:new_body
-      ~return_type:<:ctyp< $lid:type_name$ >>
-      opt_args in
-
+   
     (* return single binding of all the functions for this type *)
-    Ast.biAnd_of_list [init_db_binding; new_binding]
-  ) ts) in
+    Ast.biAnd_of_list [init_db_binding]
+  ) tables) in
   <:str_item< value $fn_bindings$ >>
 
 (* Register the persist keyword with type-conv *)
@@ -258,13 +138,20 @@ let () =
     sql_parms
     (fun tds args ->
       prerr_endline "in add_generator_with_arg: persist";
+      let _loc = Loc.ghost in
+      let ts = parse_typedef _loc tds in
+      let env = List.fold_left (fun env t ->
+        let f = { Types.f_id = t.Types.td_id; f_mut = false; f_typ = t.Types.td_typ } in
+        Sql_types.process env "__top" f;
+      ) Sql_types.empty_env ts in
+      prerr_endline (Sql_types.string_of_env env);
       match tds, args with
       |_, None ->
         Loc.raise (Ast.loc_of_ctyp tds) (Stream.Error "pa_sql_orm: arg required")
       |_, Some name ->
         let _loc = Loc.ghost in
         <:str_item<
-          $construct_typedefs tds$ ;
-          $construct_funs tds$
+          $construct_typedefs env$;
+          $construct_funs env$
         >>
       )
