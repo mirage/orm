2aecad369d30ff4c5bd3ef8617d7664cb08f2286
Anil Madhavapeddy <anil@recoil.org>
2009-08-18 00:36:53 +0100

    in the SQL types, also generate the hidden and automatic fields and mark them, and also keep track of children of tables (for autogenerated ones for lists/arrays/variants/etc)

--- sql_types.ml
+++ sql_types.ml
@@ -22,6 +22,12 @@ type sql_type =
   |Int     |Real
   |Text    |Blob
   |Null    |Foreign of string
+  |Auto_id     (* auto incrementing id field primary key *)
+  |Hidden_list (* used to mark a list in the original table *)
+
+type table_type =
+  |Exposed        (* table is an exposed external type *)
+  |Transient      (* table is internal (e.g. list) *)
 
 type env = {
   e_tables: table list;
@@ -29,10 +35,14 @@ type env = {
 and table = {
   t_name: string;
   t_fields: field list;
+  t_type: table_type;
+  t_child: string list; (* sub-tables created from this one *)
 }
 and field = {
   f_name: string;
   f_typ: sql_type;
+  f_ctyp: Types.typ option;
+  f_opt: bool;
 }
 
 (* --- String conversion functions *)
@@ -44,11 +54,13 @@ let string_of_sql_type = function
   |Int -> "INTEGER"  |Real -> "REAL"
   |Text -> "TEXT"    |Blob -> "BLOB"
   |Null -> "NULL"    |Foreign f -> "INTEGER"
+  |Auto_id -> "INTEGER PRIMARY KEY AUTOINCREMENT"
+  |Hidden_list -> "XLIST" (* should never be output in actual SQL *)
 
 let string_of_field f =
-  sprintf "%s:%s" f.f_name (string_of_sql_type f.f_typ)
+  sprintf "%s:%s%s" f.f_name (string_of_sql_type f.f_typ) (if f.f_opt then " opt" else "")
 let string_of_table t =
-  sprintf "%s: [ %s ] " t.t_name (string_map ", " string_of_field t.t_fields)
+  sprintf "%s (children=%s): [ %s ] " t.t_name (String.concat ", " t.t_child) (string_map ", " string_of_field t.t_fields)
 let string_of_env e =
   string_map "\n" string_of_table e.e_tables
 
@@ -62,14 +74,32 @@ let find_table env name =
   with
     Not_found -> None
 
+(* replace the table in the env and return new env *)
 let replace_table env table =
-  { env with e_tables =
-    table :: (List.filter (fun t -> t.t_name <> table.t_name) env.e_tables)
+  { e_tables = table :: 
+    (List.filter (fun t -> t.t_name <> table.t_name) env.e_tables)
   }
+
+let new_table ~name ~fields ~parent env =
+  (* stick in the new table *)
+  let ty = match parent with |None -> Exposed |Some _ -> Transient in
+  let env = replace_table env (match find_table env name with 
+    |None -> { t_name=name; t_fields=fields; t_type=ty; t_child=[] }
+    |Some table -> failwith (sprintf "new_table: clash %s" name) 
+  ) in
+  (* check if the table is a child and update parent child list if so *)
+  match parent with
+    |None -> env
+    |Some t -> begin
+      match find_table env t with
+      |None -> failwith (sprintf "couldnt find parent table %s" t)
+      |Some ptable -> replace_table env { ptable with t_child=name::ptable.t_child }
+    end
 
 exception Field_name_not_unique
 (* add field to the specified table, and return a modified env *)
-let add_field env t field =
+let add_field ~opt ~ctyp env t field_name field_type =
+  let field = { f_name=field_name; f_typ=field_type; f_opt=opt; f_ctyp=ctyp } in
   match find_table env t with
   |Some table -> begin
     (* sanity check that the name is unique in the field list *)
@@ -84,42 +114,77 @@ let add_field env t field =
     prerr_endline (sprintf "warning: add_field: %s:f U %s:T failed" field.f_name t);
     env
 
+(* --- Accessor functions to filter the environment *)
+
+(* list of tables for top-level code generation *)
+let exposed_tables env =
+  List.filter (fun t ->
+     t.t_type = Exposed
+   ) env.e_tables
+
+(* list of fields suitable for external ocaml interface *)
+let exposed_fields env t =
+  match find_table env t with
+  |None -> 
+    failwith (sprintf "internal error: exposed fields table '%s' not found" t)
+  |Some table ->
+    List.fold_left (fun a f ->
+      match f.f_ctyp with 
+      |None -> a
+      |Some ctyp -> (ctyp, f) :: a
+    ) [] table.t_fields
+
+(* list of fields suitable for SQL statements *)
+let sql_fields env t =
+  match find_table env t with
+  |None ->
+    failwith (sprintf "internal error: exposed fields table '%s' not found" t)
+  |Some table -> begin
+    List.fold_left (fun a f ->
+      match f.f_typ with 
+      |Hidden_list -> a
+      |_ -> f :: a
+    ) [] table.t_fields
+  end
+ 
 (* --- Process functions to convert OCaml types into SQL *)
 
 exception Type_not_allowed of string
-let rec process env t ml_field =
+let rec process ?(opt=false) ?(ctyp=None) env t ml_field =
   let n = ml_field.Types.f_id in
-  let table = find_table env t in
+  let ctyp = Some ml_field.Types.f_typ in
   match ml_field.Types.f_typ with
   (* basic types *)
-  |Types.Unit _        -> add_field env t { f_name=n; f_typ=Int  }
-  |Types.Int  _        -> add_field env t { f_name=n; f_typ=Int  }
-  |Types.Int32 _       -> add_field env t { f_name=n; f_typ=Int  }
-  |Types.Int64 _       -> add_field env t { f_name=n; f_typ=Int  }
-  |Types.Float _       -> add_field env t { f_name=n; f_typ=Real }
-  |Types.Bool _        -> add_field env t { f_name=n; f_typ=Int  }
-  |Types.Char _        -> add_field env t { f_name=n; f_typ=Int  }
-  |Types.String _      -> add_field env t { f_name=n; f_typ=Text }
+  |Types.Unit _        -> add_field ~opt ~ctyp env t n Int
+  |Types.Int  _        -> add_field ~opt ~ctyp env t n Int
+  |Types.Int32 _       -> add_field ~opt ~ctyp env t n Int
+  |Types.Int64 _       -> add_field ~opt ~ctyp env t n Int
+  |Types.Float _       -> add_field ~opt ~ctyp env t n Real
+  |Types.Bool _        -> add_field ~opt ~ctyp env t n Int
+  |Types.Char _        -> add_field ~opt ~ctyp env t n Int
+  |Types.String _      -> add_field ~opt ~ctyp env t n Text 
   (* complex types *)
   |Types.Apply(_,[],id,[]) ->
-    add_field env t { f_name=n ^ "_id"; f_typ=Foreign id }
+    add_field ~opt ~ctyp env t (n ^ "_id") (Foreign id)
   |Types.Record (_,fl)
   |Types.Object (_,fl) -> 
     (* add an id to the current table *)
-    let env = add_field env t {f_name=n ^ "_id"; f_typ=Foreign n} in
+    let env = add_field ~opt ~ctyp env t (n ^ "_id") (Foreign n) in
     (* add a new table to the environment *)
-    let env = replace_table env {t_name=n; t_fields=[]} in
+    let env = new_table ~name:n ~fields:[] ~parent:None env in
     (* process the fields in the new record *)
+    let env = add_field ~opt:false ~ctyp:None env t "id" Auto_id in
     List.fold_left (fun env field -> process env n field) env fl
   |Types.Array (_,ty)
   |Types.List (_,ty)   ->
     (* create a new table for the list lookup *)
     let t' = sprintf "%s_%s_list" t n in
     (* add table to env to represent the list *)
-    let env = replace_table env { t_name=t'; t_fields=[] } in
+    let env = new_table ~name:t' ~fields:[] ~parent:(Some t) env in
     (* add in the list fields to the new table *)
-    let env = add_field env t' { f_name= t ^ "_id"; f_typ=Foreign t } in
-    let env = add_field env t' { f_name= "_idx"; f_typ=Int } in
+    let env = add_field ~opt ~ctyp env t n Hidden_list in
+    let env = add_field ~opt ~ctyp env t' (t ^ "_id") (Foreign t) in
+    let env = add_field ~opt ~ctyp env t' "_idx" Int in
     process env t' { ml_field with Types.f_typ=ty }
   |Types.Tuple (_,tyl) ->
     let i = ref 0 in
@@ -128,16 +193,17 @@ let rec process env t ml_field =
       process env t { ml_field with Types.f_id=sprintf "%s%d" n !i; f_typ=ty }
     ) tyl env
   |Types.Option (_,ty) ->
-    process env t { ml_field with Types.f_typ=ty }
+    process ~opt:true env t { ml_field with Types.f_typ=ty }
   |Types.Variant (loc, itl) -> 
     (* Just unoptimised with a unique set of columns per branch at the moment *)
-    let env = replace_table env { t_name=n; t_fields=[] } in
-    let env = add_field env n { f_name="_idx"; f_typ=Int } in
+    let env = new_table ~name:n ~fields:[] ~parent:(Some t) env in
+    let env = add_field ~opt ~ctyp env n "_idx" Int in
     List.fold_right (fun (id,tyl) env ->
-       let n' = sprintf "%s_%s" n id in
        process env n { ml_field with Types.f_id=id; f_typ=Types.Tuple(loc, tyl) }
     ) itl env
   (* types we dont handle for the moment *)
+  |Types.Ref _
+  |Types.PolyVar _
   |Types.Var _
   |Types.Abstract _ 
   |Types.Apply _
@@ -146,3 +212,4 @@ let rec process env t ml_field =
 let process_top t =
   let env = { e_tables = [] } in
    process env "top" t
+
