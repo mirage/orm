4efda47a3ef4e2a464b075246dd39f0f61d0d91d
Anil Madhavapeddy <anil@recoil.org>
2009-08-24 22:12:59 +0100

    make the variant test save to a db

--- lib_test/variant.ml
+++ lib_test/variant.ml
@@ -6,8 +6,14 @@ type t =
 and
 x = {
   foo: t;
-  bar: int;
+  o: int;
 }
 with persist()
 
+let _ = 
+  let db = orm_init_db "variant.db" in
+  let x1 = x_new ~foo:Foo ~o:100 db in
+  let x2 = x_new ~foo:(Bar 1) ~o:101 db in
+  let x3 = x_new ~foo:(Xyz "hello") ~o:102 db in
+  List.iter (fun x -> Printf.printf "saved: %Lu\n%!" x#save) [x1;x2;x3]
 
