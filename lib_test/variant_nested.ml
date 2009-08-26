aa46427a1bf4709b48a9ee8eeb3a46c6268427e0
Anil Madhavapeddy <anil@recoil.org>
2009-08-26 14:02:53 +0100

    add a few more test cases of multiple definitions and recursively nested variants

---
+++ lib_test/variant_nested.ml
@@ -0,0 +1,24 @@
+type n =
+  |Non of int
+  |Ntwo
+  |Nthree of char
+  |Nfour of x
+and x = 
+  |Xone
+  |Xtwo of n
+  |Xthree of int
+and
+t = {
+  foo: x;
+  bar: n;
+  xyz: int;
+}
+with persist()
+
+let _ = 
+  let db = orm_init_db "variant_nested.db" in
+  let t1 = t_new ~foo:Xone ~bar:Ntwo  ~xyz:99 db in
+  let t2 = t_new ~foo:(Xtwo (Nfour Xone)) ~bar:(Nthree 'x') ~xyz:66 db in
+  let t3 = t_new ~foo:(Xtwo (Nthree 'z')) ~bar:(Non 111) ~xyz:88 db in
+  List.iter (fun x -> Printf.printf "saved: %Lu\n%!" x#save) [t1;t2;t3]
+
