75a8fa7be547ce05a026726a6fd2e0f8249b1219
Anil Madhavapeddy <anil@recoil.org>
2009-08-28 17:49:23 +0100

    add in a test which has both a foreign and variant type

---
+++ lib_test/foreign_and_variant.ml
@@ -0,0 +1,26 @@
+TYPE_CONV_PATH "Foreign_and_variant"
+
+type v =
+|Alice
+|Bob of int
+|Charlie of (int * int64) option
+and t = {
+  foo: string;
+  bar: int64;
+  xyz: v option;
+}
+and x = {
+  first: t;
+  second: t;
+  third: int;
+}
+with persist ()
+
+let _ =
+  let db = orm_init_db "foreign.db" in
+  let t1 = t_new ~foo:"hello" ~bar:100L ~xyz:None db in
+  let t2 = t_new ~foo:"word"  ~bar:200L ~xyz:(Some (Bob 1)) db in
+  let t3 = t_new ~foo:"fgfg"  ~bar:300L ~xyz:(Some (Charlie (Some (89,90L)))) db in
+  let x1  = x_new ~first:t1 ~second:t2 ~third:6 db in
+  let x2  = x_new ~first:t1 ~second:t3 ~third:7 db in
+  prerr_endline (Printf.sprintf "saved: %Lu %Lu %Lu %Lu\n%!" x1#save x2#save x1#save x2#save)
