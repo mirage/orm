eb3e20e383c24f20c4e70ebd2ef4cffdbd1e63ee
Thomas Gazagnaire <thomas@gazagnaire.com>
2009-10-28 21:07:58 +0000

    Update bug.ml. I cannot reproduce that bug with sexplib so I believe this is something which comes from our side. But no idea where to look now ...

--- lib_test/bug.ml
+++ lib_test/bug.ml
@@ -1,11 +1,12 @@
-(*pp camlp4o -I ../lib -I `ocamlfind query type-conv` pa_type_conv.cmo pa_orm.cma *)
+TYPE_CONV_PATH "bug"
 
-type t = { int: x } with orm ()
+type x = { foo : float } with orm ()
+type t = { x: x } with orm ()
 
 (*
 make -f Makefile.debug p_bug
-camlp4orf -I/Users/user/thomasga/godi/lib/ocaml/pkg-lib/type-conv -I/Users/user/thomasga/godi/lib/ocaml/site-lib/sqlite3 -I ../lib pa_type_conv.cmo -parser ../lib/pa_orm.cma -printer o bug.ml
-File "bug.ml", line 3, characters 25-28:
+camlp4o -I/Users/user/thomasga/godi/lib/ocaml/pkg-lib/type-conv -I/Users/user/thomasga/godi/lib/ocaml/site-lib/sqlite3 -I ../lib pa_type_conv.cmo -parser ../lib/pa_orm.cma -printer o bug.ml
+File "bug.ml", line 4, characters 23-26:
 Failure: "Pa_type_conv: 'orm' is not a supported type generator."
 make[1]: *** [p_bug] Error 2
 make: *** [p_bug] Error 2
