c3e7505fbb3d41cef6847a4e0d3c6d9e0306b365
Anil Madhavapeddy <anil@recoil.org>
2009-10-31 08:49:33 +0000

    hook in the hash generator properly into the ORM; now it lives in hash/hash_test and is separate from the ORM itself

---
+++ hash_test/all.ml
@@ -0,0 +1,73 @@
+(*pp camlp4o -I ../hash -I `ocamlfind query type-conv` pa_type_conv.cmo pa_hash.cma *)
+
+TYPE_CONV_PATH "All"
+
+type p =
+ |One of int array
+ |Two of t
+ |Three of x option list
+
+and pp = [ `Poly1 | `Poly2 | `Poly3 of int ]
+
+and t = {
+  t1: int;
+  mutable t2: string;
+  t3: x
+}
+
+and x = {
+  x1: t array;
+  x2: int64
+}
+
+and
+f = {
+  mutable f1: int;
+  mutable f2: string list;
+  f3: string;
+  f4: int64;
+  f5: char array;
+}
+
+and tu = ( int  * f * pp ) 
+
+and o = < x: int; y: string; z: (int -> string) >
+
+with hash
+
+module FH = Hashtbl.Make(struct
+  let hash = Hash.f
+  let equal = (=)
+  let compare = compare
+  type t = f
+end)
+
+let rs () =
+  Random.self_init ();
+  let len = Random.int 30 in
+  let s = String.create len in
+  for i = 0 to len - 1 do
+    String.set s i (Char.chr (Random.int 25 + 97))
+  done;
+  s
+
+open OUnit
+
+let test_replace () =
+  let h = FH.create 1 in
+  let fg () = { f1=(Random.int 100000); f2=[rs(); rs(); rs()]; f3=rs(); f4=(Random.int64 1000000L); f5=[|'a';'b'|] } in
+  for i = 1 to 10000 do
+    let f = fg () in
+    FH.add h f ();
+    "hash len ok" @? (FH.length h = i);
+    FH.replace h f ();
+    "hash len ok after replace" @? (FH.length h = i);
+    f.f1 <- Random.int 100000;
+    f.f2 <- [ rs (); rs (); rs ()];
+    FH.replace h f ();
+    "hash len ok after replace mutate" @? (FH.length h = i);
+  done
+
+let suite = [
+  "all_f_replace" >::  test_replace
+]
--- lib_util/p4_hash.mli
+++
@@ -1,6 +0,0 @@
-val gen1 :
-  envfn:(string -> Camlp4.PreCast.Syntax.Ast.ctyp) ->
-  Camlp4.PreCast.Syntax.Ast.ctyp -> Camlp4.PreCast.Syntax.Ast.expr
-val gen :
-  ?fun_name:(string -> string) ->
-  Camlp4.PreCast.Syntax.Ast.ctyp -> Camlp4.PreCast.Syntax.Ast.str_item
