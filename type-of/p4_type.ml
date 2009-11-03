08891c03941c11de15266a2dab47ba7029c89128
Thomas Gazagnaire <thomas@gazagnaire.com>
2009-11-03 15:45:36 +0000

    Fix-up the type generation

--- type-of/p4_type.ml
+++ type-of/p4_type.ml
@@ -33,20 +33,23 @@ let list_of_ctyp_decl tds =
 (* For each type declaration in tds, returns the corresponding unrolled Type.t.        *)
 (* The remaining free variables in the type corresponds to external type declarations. *)
 let create tds : (loc * string * t) list =
-  let ctyps = list_of_ctyp_decl tds in
-  let all_vars = List.map (fun (_,v,_) -> v) ctyps in
-  let table = Hashtbl.create 16 in
+  let bind v t = if List.mem v (free_vars t) then Rec (v, t) else t in
+  let tablefn = Hashtbl.create 16 in
+  let register name fn = Hashtbl.replace tablefn name fn in
+  let apply name arg = bind name ((Hashtbl.find tablefn name) arg) in
+  let exists name = Hashtbl.mem tablefn name in
+
   let rec aux bind_vars ctyp =
     match ctyp with
-    | <:ctyp< unit >> -> Unit
-    | <:ctyp< int >> -> Int
-    | <:ctyp< int32 >> -> Int32
-    | <:ctyp< int64 >> -> Int64
-    | <:ctyp< float >> -> Float
-    | <:ctyp< bool >> -> Bool
-    | <:ctyp< char >> -> Char
-    | <:ctyp< string >> -> String
-    | <:ctyp< option $ty$ >> -> Option (aux bind_vars ty)
+    | <:ctyp< unit >>         -> Unit
+    | <:ctyp< int >>          -> Int
+    | <:ctyp< int32 >>        -> Int32
+    | <:ctyp< int64 >>        -> Int64
+    | <:ctyp< float >>        -> Float
+    | <:ctyp< bool >>         -> Bool
+    | <:ctyp< char >>         -> Char
+    | <:ctyp< string >>       -> String
+    | <:ctyp< option $ty$ >>  -> Option (aux bind_vars ty)
     | <:ctyp< ( $tup:tp$ ) >> -> Product (List.map (aux bind_vars) (list_of_ctyp tp []))
     | <:ctyp< list $ctyp$ >>
     | <:ctyp< array $ctyp$ >> -> Collection (aux bind_vars ctyp)
@@ -67,12 +70,13 @@ let create tds : (loc * string * t) list =
           | <:ctyp< $lid:id$ : $t$ >>         -> (id, I, aux bind_vars t) :: accu
           | _                                 -> failwith "unexpected AST" in
         Named_product (fn []  fields)
-    | <:ctyp< $lid:id$ >> when not (List.mem id all_vars) || List.mem id bind_vars -> Var id
-    | <:ctyp< $lid:id$ >> -> (Hashtbl.find table id) (id :: bind_vars)
-    | x -> failwith "unknown type" in
-  List.iter (fun (loc, name, ctyp) -> Hashtbl.replace table name (fun bind_vars -> aux bind_vars ctyp)) ctyps;
-  let types = List.map (fun (loc, name, ctyp) -> loc, name, (Hashtbl.find table name) [name]) ctyps in
-  List.map (fun (loc, name, t) -> loc, name, if List.mem name (free_vars t) then Rec (name, t) else t) types
+    | <:ctyp< $lid:id$ >> when not (exists id) || List.mem id bind_vars -> Var id
+    | <:ctyp< $lid:id$ >>     -> apply id (id :: bind_vars)
+    | x                       -> failwith "unknown type" in
+
+  let ctyps = list_of_ctyp_decl tds in
+  List.iter (fun (loc, name, ctyp) -> register name (fun bind_vars -> aux bind_vars ctyp)) ctyps;
+  List.map (fun (loc, name, ctyp) -> loc, name, apply name [name]) ctyps
 
 let make_name t =
   "type_of_"^t
@@ -84,30 +88,30 @@ let gen tds =
     let freev = free_vars t in
     let rec aux = function
     | Var v when not (List.mem v freev) -> <:expr< $lid:make_name v$ () >>
-    | Var v                             -> <:expr< Var t >>
-    | Rec (v, t)                        -> <:expr< Rec (v, $aux t$) >>
-    | Unit                              -> <:expr< Unit >>
-    | Int                               -> <:expr< Int >>
-    | Int32                             -> <:expr< Int32 >>
-    | Int64                             -> <:expr< Int64 >>
-    | Float                             -> <:expr< Float >>
-    | Bool                              -> <:expr< Bool >>
-    | Char                              -> <:expr< Char >>
-    | String                            -> <:expr< String >>
-    | Option t                          -> <:expr< Option $aux t$ >>
-    | Product tl                        -> <:expr< Product $List.fold_left (fun accu x -> <:expr< [ $aux x$ :: $accu$ ] >>) <:expr< [] >> tl$ >>
-    | Collection t                      -> <:expr< Collection $aux t$ >>
+    | Var v                             -> <:expr< Orm.Type.Var $str:v$ >>
+    | Rec (v, t)                        -> <:expr< Orm.Type.Rec ($str:v$, $aux t$) >>
+    | Unit                              -> <:expr< Orm.Type.Unit >>
+    | Int                               -> <:expr< Orm.Type.Int >>
+    | Int32                             -> <:expr< Orm.Type.Int32 >>
+    | Int64                             -> <:expr< Orm.Type.Int64 >>
+    | Float                             -> <:expr< Orm.Type.Float >>
+    | Bool                              -> <:expr< Orm.Type.Bool >>
+    | Char                              -> <:expr< Orm.Type.Char >>
+    | String                            -> <:expr< Orm.Type.String >>
+    | Option t                          -> <:expr< Orm.Type.Option $aux t$ >>
+    | Product tl                        -> <:expr< Orm.Type.Product $List.fold_left (fun accu x -> <:expr< [ $aux x$ :: $accu$ ] >>) <:expr< [] >> tl$ >>
+    | Collection t                      -> <:expr< Orm.Type.Collection $aux t$ >>
     | Named_sum ts                      -> 
       let rec fn accu = function
       | []          -> accu
       | (n, t) :: l -> <:expr< [ ( $str:n$, $List.fold_left (fun accu x -> <:expr< [ $aux x$ :: $accu$ ] >>) <:expr< [] >> t$ ) :: $fn accu l$ ] >> in
-      <:expr< Named_sum $fn <:expr< [] >> ts$ >>
+      <:expr< Orm.Type.Named_sum $fn <:expr< [] >> ts$ >>
     | Named_product ts                  ->
       let rec fn accu = function
       | []              -> accu
-      | (n, M, t) :: l -> <:expr< [ ($str:n$, M, $aux t$) :: $fn accu l$ ] >>
-      | (n, I, t) :: l -> <:expr< [ ($str:n$, I, $aux t$) :: $fn accu l$ ] >> in
-      <:expr< Named_product $fn <:expr< [] >> ts$ >>
+      | (n, M, t) :: l -> <:expr< [ ($str:n$, Orm.Type.M, $aux t$) :: $fn accu l$ ] >>
+      | (n, I, t) :: l -> <:expr< [ ($str:n$, Orm.Type.I, $aux t$) :: $fn accu l$ ] >> in
+      <:expr< Orm.Type.Named_product $fn <:expr< [] >> ts$ >>
     in
     <:binding< $lid:make_name name$ = $aux t$ >>
   in
