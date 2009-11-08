5394969bb9334143879b45f9bbc3bc36af3b9baa
Thomas Gazagnaire <thomas@gazagnaire.com>
2009-11-08 17:11:37 +0000

    [value] improve automatic generation of random cyclic values, for the unit tests.

--- value/test/all.ml
+++ value/test/all.ml
@@ -57,54 +57,50 @@ let array v =
 
 let list v = Array.to_list (array v)
 
-let p t x =
-	match Random.int 3 with
-	| 0 -> One (string (), array int, char (), bool (), list float)
-	| 1 -> Two t
-	| 2 -> Three (list (fun () -> option (fun () -> x)))
-	| _ -> assert false
-
-let pp () =
-	match Random.int 3 with
-	| 0 -> `Poly1
-	| 1 -> `Poly2
-	| 3 -> `Poly3 (int ())
-	| _ -> assert false
-
-let t x = { t1 = int (); t2 = string (); t3 = x }
-
-let x t = { x1 = array (fun () -> t); x2 = int64 () }
-		
-let f () = { f1 = int (); f2 = list string; f3 = string (); f4 = int64 (); f5 = array char }
-		
-let tu f pp  = ( int (), f, pp)
-
-let o () : o = object method x = f () method y = string () method z = (fun i -> string () ^ string_of_int i) end
-
 let rec x1 = { x1 = [| t1; t2 |]; x2 = int64 () }
 and t1 = { t1 = int (); t2 = string (); t3 = x1 }
 and t2 = { t1 = int (); t2 = string (); t3 = x1 }
 
+let rec p () =
+	match Random.int 3 with
+	| 0 -> One (string (), array int, char (), bool (), list float)
+	| 1 -> Two (t ())
+	| 2 -> Three (list (fun () -> option x))
+	| _ -> assert false
+
+and pp () =
+	match Random.int 3 with
+	| 0 -> `Poly1
+	| 1 -> `Poly2
+	| 2 -> `Poly3 (int ())
+	| _ -> assert false
+
+and t () = if Random.int 10 > 1 then t1 else { t1 = int (); t2 = string (); t3 = x () }
+
+and x () = if Random.int 10 > 1 then x1 else { x1 = array t; x2 = int64 () }
+		
+and f () = { f1 = int (); f2 = list string; f3 = string (); f4 = int64 (); f5 = array char }
+		
+and tu ()  = ( int (), f (), pp ())
+
+let o () : o = object method x = f () method y = string () method z = (fun i -> string () ^ string_of_int i) end
+
 let test_marshall () =
-	for i = 1 to 2 do begin
+	for i = 1 to 1000 do begin
+		let p = p () in
+		let pp = pp () in
+		let t = t () in
+		let x = x () in
 		let f = f () in
+		let tu = tu () in
 		let o = o () in
-
-		Printf.printf "x1=%s\n%!" (Value.to_string (value_of_x x1));
-		Printf.printf "t1=%s\n%!" (Value.to_string (value_of_t t1));
-		Printf.printf "t2=%s\n%!" (Value.to_string (value_of_t t2));
-		"EQ f" @? (f = f_of_value (value_of_f f));
-		"EQ t1 (1)" @? (value_of_t t1 = value_of_t t1);
-		"EQ t1" @? (value_of_t t1 = value_of_t (t_of_value (value_of_t t1)));
-(*		"EQ t" @? (value_of_t t2 = value_of_t (t_of_value (value_of_t t2)));
-		"EQ t" @? (value_of_x x1 = value_of_x (x_of_value (value_of_x x1)));*)
-(*
-		"EQ p" @? (p = p_of_value (value_of_p p));
-		"EQ pp" @? (pp = pp_of_value (value_of_pp pp));
-
-		"EQ tu" @? (tu = tu_of_value (value_of_tu tu));
-*)
-
+		"EQ p" @? (value_of_p p = value_of_p (p_of_value (value_of_p p)));
+		"EQ pp" @? (value_of_pp pp = value_of_pp (pp_of_value (value_of_pp pp)));
+		"EQ t" @? (value_of_t t = value_of_t (t_of_value (value_of_t t)));
+		"EQ x" @? (value_of_x x = value_of_x (x_of_value (value_of_x x)));
+		"EQ f" @? (value_of_f f = value_of_f (f_of_value (value_of_f f)));
+		"EQ tu" @? (value_of_tu tu = value_of_tu (tu_of_value (value_of_tu tu)));
+		"EQ o" @? (ignore( value_of_o (o_of_value (value_of_o o))); true); (* Marshaled functions cannot be compared *)
 	end done
 
 let suite = [
