d6c96523bfffbe4c10bd0e0ab20b5441fa034201
Thomas Gazagnaire <thomas@gazagnaire.com>
2009-11-09 00:49:57 +0000

    [lib] the creation of tables is almost done ...

--- lib/sql_init.ml
+++ lib/sql_init.ml
@@ -21,6 +21,11 @@ open Sql_backend
 open Sqlite3
 
 exception Sql_process_error of string
+let process_error s = raise (Sql_process_error s)
+
+let list n = sprintf "%s__list" n
+let tuple n i = sprintf "%s__%i" n i
+let row n r i = sprintf "%s__%s__%i" n r i
 
 let create_types_table db t =
   let create = "CREATE TABLE IF NOT EXISTS __types__ (n TEXT, t TEXT)" in
@@ -30,11 +35,11 @@ let create_types_table db t =
   (* Create the __types__ table *)
   db_must_ok db (fun () -> exec db.db create);
   
-  (* Populate the table with the decomposition of t *)
-  List.iter (fun (n, t) ->
+  (* Insert the type t of table n into __types__ *)
+  let rec process (n, t) =
     let stmt = prepare db.db select in
     db_must_bind db stmt 1 (Data.TEXT (Type.to_string t));
-    match step_map db stmt (fun stmt -> column stmt 0) with
+    begin match step_map db stmt (fun stmt -> column stmt 0) with
     | [] ->
       let stmt = prepare db.db insert in
       db_must_bind db stmt 1 (Data.TEXT n);
@@ -43,8 +48,21 @@ let create_types_table db t =
     | [Data.TEXT x] ->
       if not (Type.is_subtype_of t (Type.of_string x)) then
 	raise (Type.Subtype_error (Type.to_string t, x))
-    | _ -> raise (Sql_process_error "create_types_sig")
-    ) (Type.types_of t)
+    | _ -> process_error "create_types_table"
+    end;
+    aux ~n t
+  
+  (* Call process for any sub-table of t *)
+  and aux ?n t = match t with
+    | Type.Ext (n, t) | Type.Rec (n, t) -> process (n, t)
+    | Type.Enum t ->
+      begin match n with
+      | None -> process_error "create_types_table"
+      | Some n -> process (list n, t)
+      end
+    | _ -> () in
+
+  aux t
 
 let create_depends_table db t =
   let create = "CREATE TABLE IF NOT EXISTS __links__ (parent TEXT, child TEXT)" in
@@ -54,28 +72,84 @@ let create_depends_table db t =
   (* Create the __links__ table *)
   db_must_ok db (fun () -> exec db.db create);
 
-  (* Populate the table with the links of the decomposition of t *)
-  List.iter (fun (p, n) ->
+  (* Insert the link 'p is a parent of n' into __links__ *)
+  let rec process p (n, t) =
     let stmt = prepare db.db select in
     db_must_bind db stmt 1 (Data.TEXT p);
-    match step_map db stmt (fun stmt -> column stmt 0) with
+    begin match step_map db stmt (fun stmt -> column stmt 0) with
     | [] ->
       let stmt = prepare db.db insert in
       db_must_bind db stmt 1 (Data.TEXT p);
       db_must_bind db stmt 1 (Data.TEXT n);
       db_must_step db stmt
     | [Data.TEXT x] -> ()
-    | _ -> raise (Sql_process_error "create_links_table")
-    ) (Type.links_of t)
+    | _ -> process_error "create_links_table"
+    end;
+    aux ~p:n t
 
-(* let process_field = function
-  | Unit -> "NULL"
-  | Int | Int32 | Int64 | Char | Bool -> "INT"
-  | Float -> "REAL"
-  | String -> "TEXT"
-  |  -> "INT"
+  (* Call process for any pair of sub-tables of t *)
+  and aux ?p t = match t with 
+    | Type.Ext (n, t) | Type.Rec (n, t) ->
+       begin match p with
+       | None -> process_error "create_links_table"
+       | Some p -> process p (n, t)
+       end
+    | Type.Enum t ->
+      begin match p with
+      | None -> process_error "create_links_table"
+      | Some p -> process p (list p, t)
+      end;
+    | _ -> () in
 
-let process_table = function
+  aux t
+
+let foldi fn accu l =
+  let accu, _ = List.fold_left (fun (accu, i) x -> fn accu i x, i + 1) (accu, 0) l in accu
+
+let mapi fn l = foldi (fun accu i x -> fn i x :: accu) [] l
+let map_strings sep fn sl = String.concat sep (List.map fn sl)
+let map_stringsi sep fn sl = String.concat sep (mapi fn sl)
+
+let create_tables indices db t =
+
+  let process s = db_must_ok db (fun () -> exec db.db s) in
+
+  let rec field name = function 
+    | Unit | Int | Int32 | Int64 | Char | Bool
+    | Var _ | Rec _ | Ext _
+               -> sprintf "%s INT" name
+    | Enum t   -> table ~name (Enum t); sprintf "%s INT" name
+    | Float    -> sprintf "%s REAL" name
+    | String   -> sprintf "%s TEXT" name
+    | Arrow _  -> sprintf "%s BLOB" name
+    | Tuple tl -> map_stringsi ", " (fun i t -> table ~name t; field (tuple name i) t) tl
+    | Dict tl  -> map_strings ", " (fun (m,_,t) -> table ~name t; field m t) tl
+    | Sum tl   -> map_strings "__row__ TEXT, %s" (fun (r, tl) -> map_stringsi "," (fun i t -> table ~name t; field (row name r i) t) tl) tl
+    | Option t -> field name t
+
+  and table ?name t = match t with
+    | Type.Ext (n, t) | Type.Rec (n, t) ->
+      process (sprintf "CREATE TABLE IF NOT EXISTS %s (__id__ PRIMARY KEY AUTOINCREMENT, %s)" n (field n t))
+    | Type.Enum t ->
+      begin match name with
+      | None -> process_error "create_tables"
+      | Some n ->
+        process (sprintf "CREATE TABLE IF NOT EXISTS %s (__id__ PRIMARY KEY AUTOINCREMENT, __idx__ INT, PRIMARY KEY (__id__, __idx___), %s)" n (field n t))
+      end;
+    | _ -> raise (Sql_process_error "create_tables") in
+
+  (* Create all the sub-table of t*)
+  table t;
+
+  (* Process indices *)
+  List.iter (fun (unique,t,fs) ->
+    let s = sprintf "CREATE %sINDEX IF NOT EXISTS idx_%s_%s ON %s (%s);"
+       (match unique with true -> "UNIQUE " |false -> "")
+       t (String.concat "_" fs) t (String.concat "," fs) in
+     process s
+    ) indices
+
+(*let process_table = function
   | Unit -> "CREATE TABLE IF NOT EXISTS '%s' (id )"
 
      (* Simpler case: just add a new field to the current table *)
