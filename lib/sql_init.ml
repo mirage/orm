bcbcc27f7b36aead5dcc720c7453e45eb2621e16
Thomas Gazagnaire <thomas@gazagnaire.com>
2009-11-09 23:34:01 +0000

    [lib] Fix table creation

--- lib/sql_init.ml
+++ lib/sql_init.ml
@@ -20,35 +20,45 @@ open Type
 open Sql_backend
 open Sqlite3
 
-exception Sql_process_error of string
-let process_error s = raise (Sql_process_error s)
+exception Sql_process_error of Type.t * string
+let process_error t s =
+  Printf.printf "ERROR(%s): %s\n%!" s (Type.to_string t);
+  raise (Sql_process_error (t,s))
 
 let list n = sprintf "%s__list" n
 let tuple n i = sprintf "%s__%i" n i
 let row n r i = sprintf "%s__%s__%i" n r i
 
-let create_types_table db t =
+
+
+let create_types_table env db t =
   let create = "CREATE TABLE IF NOT EXISTS __types__ (n TEXT, t TEXT)" in
   let select = "SELECT t FROM __types__ WHERE n=?" in
   let insert = "INSERT INTO __types__ (n,t) VALUES (?,?)" in
 
   (* Create the __types__ table *)
+  debug env `Sql "init" create;
   db_must_ok db (fun () -> exec db.db create);
   
   (* Insert the type t of table n into __types__ *)
   let rec process (n, t) =
+    debug env `Sql "init" select;
     let stmt = prepare db.db select in
+    debug env `Bind "init" (Type.to_string t);
     db_must_bind db stmt 1 (Data.TEXT (Type.to_string t));
     begin match step_map db stmt (fun stmt -> column stmt 0) with
     | [] ->
+      debug env `Sql "init" insert;
       let stmt = prepare db.db insert in
+      debug env `Bind "init" n;
       db_must_bind db stmt 1 (Data.TEXT n);
+      debug env `Bind "init" (Type.to_string t);
       db_must_bind db stmt 2 (Data.TEXT (Type.to_string t));
       db_must_step db stmt
     | [Data.TEXT x] ->
       if not (Type.is_subtype_of t (Type.of_string x)) then
-	raise (Type.Subtype_error (Type.to_string t, x))
-    | _ -> process_error "create_types_table"
+        raise (Type.Subtype_error (Type.to_string t, x))
+    | _ -> process_error t "create_types_table:1"
     end;
     aux ~n t
   
@@ -57,33 +67,38 @@ let create_types_table db t =
     | Type.Ext (n, t) | Type.Rec (n, t) -> process (n, t)
     | Type.Enum t ->
       begin match n with
-      | None -> process_error "create_types_table"
+      | None -> process_error t "create_types_table:2"
       | Some n -> process (list n, t)
       end
     | _ -> () in
 
   aux t
 
-let create_depends_table db t =
+let create_links_table env db t =
   let create = "CREATE TABLE IF NOT EXISTS __links__ (parent TEXT, child TEXT)" in
   let select = "SELECT child FROM __links__ WHERE parent=? AND child=?" in
   let insert = "INSERT INTO __links__ (parent, child) VALUES (?,?)" in
 
   (* Create the __links__ table *)
+  debug env `Sql "init" create;
   db_must_ok db (fun () -> exec db.db create);
 
   (* Insert the link 'p is a parent of n' into __links__ *)
   let rec process p (n, t) =
+    debug env `Sql "init" select;
     let stmt = prepare db.db select in
     db_must_bind db stmt 1 (Data.TEXT p);
     begin match step_map db stmt (fun stmt -> column stmt 0) with
     | [] ->
+      debug env `Sql "init" insert;
       let stmt = prepare db.db insert in
+      debug env `Bind "init" p;
       db_must_bind db stmt 1 (Data.TEXT p);
+      debug env `Bind "init" n;
       db_must_bind db stmt 1 (Data.TEXT n);
       db_must_step db stmt
     | [Data.TEXT x] -> ()
-    | _ -> process_error "create_links_table"
+    | _ -> process_error t "create_links_table:1"
     end;
     aux ~p:n t
 
@@ -91,12 +106,12 @@ let create_depends_table db t =
   and aux ?p t = match t with 
     | Type.Ext (n, t) | Type.Rec (n, t) ->
        begin match p with
-       | None -> process_error "create_links_table"
+       | None -> aux ~p:n t
        | Some p -> process p (n, t)
        end
     | Type.Enum t ->
       begin match p with
-      | None -> process_error "create_links_table"
+      | None -> process_error t "create_links_table:2"
       | Some p -> process p (list p, t)
       end;
     | _ -> () in
@@ -111,17 +126,17 @@ let map_strings sep fn sl = String.concat sep (List.map fn sl)
 let map_stringsi sep fn sl = String.concat sep (mapi fn sl)
 
 let init_tables ~mode ~env ~db t =
-
   let sub_tables = ref [] in
   let process t s =
-    let sql = sprintf "CREATE TABLE IF NOT EXISTS %s (__id__ PRIMARY KEY AUTOINCREMENT, %s)" t s in
+    let sql = sprintf "CREATE TABLE IF NOT EXISTS %s (__id__ INTEGER PRIMARY KEY AUTOINCREMENT, %s)" t s in
 	sub_tables := t :: !sub_tables;
+    debug env `Sql "init" sql;
 	db_must_ok db (fun () -> exec db.db sql) in
 
   let rec field name = function 
     | Unit | Int | Int32 | Int64 | Char | Bool
     | Var _ | Rec _ | Ext _
-               -> sprintf "%s INT" name
+               -> sprintf "%s INTEGER" name
     | Enum t   -> table ~name (Enum t); sprintf "%s INT" name
     | Float    -> sprintf "%s REAL" name
     | String   -> sprintf "%s TEXT" name
@@ -135,19 +150,22 @@ let init_tables ~mode ~env ~db t =
     | Type.Ext (n, t) | Type.Rec (n, t) -> process n (field n t)
     | Type.Enum t ->
       begin match name with
-      | None   -> process_error "create_tables"
+      | None   -> process_error t "init_tables:1"
       | Some n -> process n (sprintf "__idx__ INT, PRIMARY KEY (__id__, __idx___), %s" (field n t))
       end;
-    | _ -> raise (Sql_process_error "create_tables") in
+    | _ -> () in
 
   (* Create all the sub-table of t*)
   table t;
+  create_types_table env db t;
+  create_links_table env db t;
 
   let process_custom_index kind (t, fs) =
     if List.mem t !sub_tables then begin
         let sql = match kind with
         | `U -> sprintf "CREATE UNIQUE INDEX IF NOT EXISTS idx_%s_%s ON %s (%s);" t (String.concat "_" fs) t (String.concat "," fs)
         | `I -> sprintf "CREATE INDEX IF NOT EXISTS idx_%s_%s ON %s (%s);" t (String.concat "_" fs) t (String.concat "," fs) in
+        debug env `Sql "init" sql;
         db_must_ok db (fun () -> exec db.db sql)
 	end in
 
