9f08ccba36680334c876d9eb10233630947bc7eb
Thomas Gazagnaire <thomas@gazagnaire.com>
2009-11-17 18:36:25 +0000

    [get] improve t_get for list/arrays. It doesn't work at the moment but hopefully very soon.

--- lib/sql_get.ml
+++ lib/sql_get.ml
@@ -29,7 +29,7 @@ let process_error v d s =
   raise (Sql_process_error (v, d, s))
 
 (* Build up the list of fields actually needed to save the row *)
-let rec parse_row ~env ~db ?(skip=false) ~id t row n =
+let rec parse_row ~env ~db ?(skip=false) ~id ~name t row n =
   match t, row.(n) with
   | T.Unit    , Data.INT 0L -> V.Null, n + 1
   | T.Int     , Data.INT i
@@ -40,24 +40,27 @@ let rec parse_row ~env ~db ?(skip=false) ~id t row n =
   | T.Bool    , Data.INT 1L  -> V.Bool true, n + 1
   | T.Float   , Data.FLOAT f -> V.Float f, n + 1
   | T.String  , Data.TEXT t  -> V.String t, n + 1
-  | T.Enum t  , Data.INT _   -> process_error t row.(n) "TODO:1 (enum_get)"
+  | T.Enum t  , Data.INT idx ->
+    let values = get_values ~env ~db ~idx (T.Ext (name, T.Enum t)) in
+    let values = snd (List.split values) in
+    V.Enum values, n + 1
   | T.Arrow _ , Data.BLOB b  -> V.Arrow b, n + 1
-  | T.Option t, Data.INT r   -> let res, j = parse_row ~env ~db ~skip:(r=0L) ~id t row (n + 1) in (if r=0L then V.Null else res), j
+  | T.Option t, Data.INT r   -> let res, j = parse_row ~env ~db ~skip:(r=0L) ~id ~name t row (n + 1) in (if r=0L then V.Null else res), j
   | T.Tuple tl, _            ->
-    let tuple, n = List.fold_left (fun (accu, i) t ->
-      let res, j = parse_row ~env ~db ~id t row i in res :: accu, j
+    let tuple, n = list_foldi (fun (accu, n1) i t ->
+      let res, n2 = parse_row ~env ~db ~id ~name:(Name.tuple_field name i) t row n1 in res :: accu, n2
       ) ([], n) tl in
     V.Tuple (List.rev tuple), n
   | T.Dict tl , _            ->
-    let dict, n = List.fold_left (fun (accu, i) (f,_,t) ->
-      let res, j = parse_row ~env ~db ~id t row i in (f, res) :: accu, j
+    let dict, n = List.fold_left (fun (accu, n1) (f,_,t) ->
+      let res, n2 = parse_row ~env ~db ~id ~name:(Name.dict_field name f) t row n1 in (f, res) :: accu, n2
       ) ([], n) tl in
     V.Dict (List.rev dict), n
   | T.Sum tl  , Data.TEXT r  ->
-    let row, n = List.fold_left (fun (accu, i) (rn, tl) ->
-      List.fold_left (fun (accu, j) t ->
-        let res, k = parse_row ~skip:(rn<>r) ~env ~db ~id t row i in (if rn<>r then accu else res :: accu), k
-        ) (accu, i) tl)
+    let row, n = List.fold_left (fun (accu, n1) (rn, tl) ->
+      list_foldi (fun (accu, n2) i t ->
+        let res, n3 = parse_row ~skip:(rn<>r) ~env ~db ~id ~name:(Name.sum_field name rn i) t row n2 in (if rn<>r then accu else res :: accu), n3
+        ) (accu, n1) tl)
       ([], n + 1) tl in
     V.Sum (r, List.rev row), n
   | T.Rec(r, s), Data.INT i  ->
@@ -76,30 +79,41 @@ let rec parse_row ~env ~db ?(skip=false) ~id t row n =
   | _ when skip              -> V.Null, n + 1
   | _                        -> process_error t row.(n) "unknown"
 
-and get_values ~env ~db ?id t =
-  let process n t =
-    let fields = field_names_of_type ~id:true t in
-    let where = match id with None -> "" | Some id -> " WHERE __id__=?" in
-    let sql = sprintf "SELECT %s FROM %s%s" (String.concat "," fields) n where in
+and get_values ~env ~db ?id ?idx t =
+  let process (name, t) =
+    let t_internal = if is_enum t then get_enum_type t else t in
+    let t_name = if is_enum t then name else "" in
+    let field_names = field_names_of_type ~id:true ~name:t_name t_internal in
+    let where =
+      (match id with None -> "" | Some id -> " WHERE __id__=?") ^
+      (match idx with None -> "" | Some id -> " WHERE __idx__=?") in
+    let sql = sprintf "SELECT %s FROM %s%s" (String.concat "," field_names) name where in
     debug env `Sql "get" sql;
     let stmt = prepare db.db sql in
+    let bind = ref 0 in
     (match id with 
       | None    -> () 
       | Some id ->
         debug env `Bind "get" (Int64.to_string id);
-        db_must_bind db stmt 1 (Data.INT id));
+        db_must_bind db stmt (incr bind; !bind) (Data.INT id));
+    (match idx with
+      | None     -> ()
+      | Some id  ->
+        debug env `Bind "get" (Int64.to_string id);
+        db_must_bind db stmt (incr bind; !bind) (Data.INT id)); 
     step_map db stmt (fun stmt ->
       let row = row_data stmt in
       let id = match row.(0) with Data.INT i -> i | _ -> failwith "TODO:4" in
-      let r, _ = parse_row ~env ~db ~id t row 1 in
-      if List.mem (n, id) (V.free_vars r) then
-        id, V.Rec ((n,id),r)
+      let r, _ = parse_row ~env ~db ~id ~name t row 1 in
+      if is_enum t then
+        id, r
+      else if List.mem (name, id) (V.free_vars r) then
+        id, V.Rec ((name,id),r)
       else
-        id, V.Ext ((n,id),r))
+        id, V.Ext ((name,id),r))
 
   in
   match t with
-  | T.Rec (n,t)
-  | T.Ext (n,t) -> process n t
-  | _           -> failwith "TODO"  
-  
+  | T.Rec (n, t)
+  | T.Ext (n, t) -> process (n, t)
+  | _            -> failwith "TODO"  
