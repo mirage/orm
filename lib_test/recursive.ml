1b960b0235086642050a0a353dd277cb0683f95d
Thomas Gazagnaire <thomas@gazagnaire.com>
2010-02-09 11:43:43 +0000

    [test] add a failing test for recursive deletion.

--- lib_test/recursive.ml
+++ lib_test/recursive.ml
@@ -1,5 +1,7 @@
 TYPE_CONV_PATH "Recursive"
 
+type y = char with orm
+
 type t = { 
   t1: string;
   t2: x option
@@ -8,13 +10,20 @@ type t = {
   x2: char
 } with orm
 
+type z = t with orm
+
+type a = { a : y } with orm
+
 open Test_utils
 open OUnit
 
 let name = "recursive.db"
 
+let vy = 'a'
 let rec vt = { t1= "hello"; t2=(Some vx) }
-and vx = { x1=(Some vt); x2='z' }
+and vx = { x1=(Some vt); x2=vy }
+let vz = vt
+let va = { a = vy }
 
 let test_init () =
   ignore(open_db t_init name);
@@ -48,15 +57,39 @@ let test_save_get () =
   let i = List.hd i in
   "physical values equal" @? ( vx == i)
 
-let test_delete () = 
-  let dbx = open_db x_init name in
+(* We have:  vz => vt <=> vx => vy <= va                                      *)
+(* Deletion should be possible if no reference exists to the recursive cycle  *)
+(* the value belongs to.                                                      *)
+(* For instance, deleting vx while vz is still in the database should not be  *)
+(* possible. However, deleting vz should delete vt and vx as well, but let vy *)
+(* is in the dabatase as va is referencing it.                                *)
+let test_delete () =
+  let dbz = open_db z_init name in
   let dbt = open_db ~rm:false t_init name in
-  x_save dbx vx;
-  "1 x in db" @? (List.length (x_get dbx) = 1);
-  "1 s in db" @? (List.length (t_get dbt) = 1);
+  let dbx = open_db ~rm:false x_init name in
+  let dby = open_db ~rm:false y_init name in
+  let dba = open_db ~rm:false a_init name in
+
+  let check (z, t, x, y, a) =
+    (Printf.sprintf "%d z in db" z) @? (List.length (z_get dbz) = z);
+    (Printf.sprintf "%d t in db" t) @? (List.length (t_get dbt) = t);
+	(Printf.sprintf "%d x in db" x) @? (List.length (x_get dbx) = x);
+	(Printf.sprintf "%d y in db" y) @? (List.length (y_get dby) = y);
+	(Printf.sprintf "%d a in db" a) @? (List.length (a_get dba) = a) in
+
+  z_save dbz vz;
+  a_save dba va;
+  
+  (* 0. basic sanity checks before doing the delete test *)
+  check (1, 1, 1, 1, 1);
+
+  (* 1. deleting vx should not be possible *)
   x_delete dbx vx;
-  "0 x in db" @? (List.length (x_get dbx) = 0);
-  "0 s in db" @? (List.length (t_get dbt) = 0)
+  check (1, 1, 1, 1, 1);
+
+  (* 2. deleting vz should delete vt and vx as well and let vy in the database *)
+  z_delete dbz vz;
+  check (0, 0, 0, 1, 1)
 
 let suite = [
   "recursive_init" >:: test_init;
