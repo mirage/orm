f3dcf7576448949ac13b2945c3841e4466592792
Anil Madhavapeddy <anil@recoil.org>
2010-02-11 18:06:50 +0000

    [build] remove the unused bits of hash/

--- hash/p4_hash.mli
+++
@@ -1,3 +0,0 @@
-val hash_of : string -> string
-val gen1 : envfn:(string -> Camlp4.PreCast.Syntax.Ast.ctyp option) ->Camlp4.PreCast.Syntax.Ast.ctyp -> Camlp4.PreCast.Syntax.Ast.expr
-val gen : Camlp4.PreCast.Syntax.Ast.ctyp -> Camlp4.PreCast.Syntax.Ast.str_item
